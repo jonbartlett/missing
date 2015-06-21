0d82186f24285460224facdf66e6bc7173259449
Jon Bartlett <mail@jaybe.net mailto:mail@jaybe.net>
2015-06-22 08:23:05 +1000

    UI fixes

--- bin/missing.rb
+++ bin/missing.rb
@@ -10,13 +10,17 @@ UI::Banner.print
 # index master library
 master_lib = Missing::Library.new()
 master_lib.build(options[:libpath])
- 
+binding.pry
+
 # index possible dups library
 dup_lib = Missing::Library.new()
 dup_lib.build(options[:duppath])
+binding.pry
 
 # compare both libraries and return new files less duplicates
 new_lib = master_lib.diff(dup_lib)
+binding.pry
 
+UI::Progress.diff_results(new_lib.photos.length, dup_lib.photos.length)
 
 binding.pry
